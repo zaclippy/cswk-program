7be11e6d0368f21ba454c9795652667cf7550271
Alexander <alexander.dixon@warwick.ac.uk>
2022-03-01 00:21:44 +0000

    Initial version

---
+++ app/Main.hs
@@ -0,0 +1,8 @@
+module Main where
+
+import Lib
+
+{- This is the main entry point to your program. -}
+
+main :: IO ()
+main = someFunc
---
+++ src/Lib.hs
@@ -0,0 +1,10 @@
+module Lib
+    ( someFunc
+    ) where
+
+{- NOTE that you are NOT obligated to keep any of the files from 
+the skeleton code, including this one. You should give your 
+modules sensible names that correspond to their contents. -}
+
+someFunc :: IO ()
+someFunc = putStrLn "someFunc"
